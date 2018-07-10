2963d849dc15fec12d6ec71f5c96ed385b7baf4d
Litton Sinha <litton@cti.local>
2018-07-10 19:06:23 +0530

    First Java program

---
+++ src/javaPractice/Variables.java
@@ -0,0 +1,13 @@
+package javaPractice;
+
+public class Variables {
+
+	public static void main(String[] args)
+	{
+		String a1 = "Java";
+		String a2 = "C";
+		System.out.println(a1 + a2);
+		
+	}
+
+}
